f19081d13a3c8efc359c1dd24747cf3ecdf3ca17
MatteoSala97 <matteo111997@gmail.com>
2023-12-21 14:59:01 +0100

    feat: js variables and prompt

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("")
+//variables
+let nomeUtente = prompt('Inserisci il tuo nome');
+let cognomeUtente = prompt('Inserisci il tuo cognome');
+let colorePreferito = prompt('Inserisci il tuo colore preferito');
+let numeroRandom = prompt('Inserisci il tuo numero random');
+
+
+console.log(nomeUtente + ' ' + cognomeUtente + ' ' + colorePreferito + numeroRandom);
+document.writeln(nomeUtente + ' ' + cognomeUtente + ' ' + colorePreferito + numeroRandom);
