cc906518e8cf6f9433e639422f2dff0ce57e350a
MatteoSala97 <matteo111997@gmail.com>
2023-12-21 15:44:56 +0100

    minor fixes

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 //variables
+
 let nomeUtente = prompt('Inserisci il tuo nome');
 let cognomeUtente = prompt('Inserisci il tuo cognome');
 let colorePreferito = prompt('Inserisci il tuo colore preferito');
 let numeroRandom = prompt('Inserisci il tuo numero random');
 
 
-console.log(nomeUtente + ' ' + cognomeUtente + ' ' + colorePreferito + numeroRandom);
-document.writeln(nomeUtente + ' ' + cognomeUtente + ' ' + colorePreferito + numeroRandom);
+console.log(nomeUtente + cognomeUtente + colorePreferito + numeroRandom);
+document.writeln(nomeUtente + cognomeUtente + colorePreferito + numeroRandom);
